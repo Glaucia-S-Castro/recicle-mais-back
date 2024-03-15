cb66fc5020413e726494d76fb897f292dbab7a48
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-03-15 13:35:09 -0300

    Commitando minhas alterações no user.service.ts

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,10 @@
-import { ConflictException, Injectable} from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { isEmail } from 'class-validator';
 import { PrismaService } from 'src/database/PrismaService';
 import { UserDTO } from './user.dto';
 
-export type User = any
+export type User = any;
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
@@ -31,14 +31,12 @@ export class UserService {
         password: hash,
       },
     });
-    const { password, ...newUser } = user;
+    const { ...newUser } = user;
     return newUser;
   }
 
-   private readonly users = this.prisma.user.findMany();
+  private readonly users = this.prisma.user.findMany();
   async findOne(fullname: string): Promise<User | undefined> {
-
     return (await this.users).find((user) => user.fullname === fullname);
   }
- 
 }

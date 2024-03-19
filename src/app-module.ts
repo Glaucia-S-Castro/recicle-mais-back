bc0801c9b166b546a4e66ac05159a3fe1591c649
Glaucia Castro <galcastrossc@gmail.com>
2024-03-18 23:41:31 -0300

    change folder name update-password

--- src/app-module.ts
+++ src/app-module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './users/register-user/user-module';
 import { AuthModule } from './users/Auth/auth-module';
 import { UpdateUserModule } from './users/update-user/update-user-module';
-import { UpdatePassModule } from './users/update-password-user/update-pass-module';
+import { UpdatePassModule } from './users/update-password/update-pass-module';
 import { DeleteUserModule } from './users/delete-user/delete-user-module';
 
 @Module({

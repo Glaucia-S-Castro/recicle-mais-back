e614b014ebe0ba050391cbd1afe969c37a49f81a
Glaucia Castro <galcastrossc@gmail.com>
2024-04-01 21:12:07 -0300

    refactoring user deletion to include confirmation before deletion (still under construction)

--- src/users/delete-user/delete-user-controller.ts
+++ src/users/delete-user/delete-user-controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import { Controller, Delete, Body, Param } from '@nestjs/common';
+import { deleteUserDTO } from './delete-user-dto';
 import { DeleteUserService } from './delete-user-service';
 import { ApiTags } from '@nestjs/swagger';
+
 @ApiTags("Delete-user")
 @Controller('delete-user')
 export class DeleteUserController {
-  constructor(private readonly deleteUserService: DeleteUserService) {}
+  constructor(private readonly deleteUserService: DeleteUserService) { }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    await this.deleteUserService.deleteUser(id);
+  async remove(@Param('id') id: string, @Body() data: deleteUserDTO) {
+    await this.deleteUserService.deleteUser(
+      id,
+      data.confirmationPhrase
+    );
     return `Usuário removido com sucesso.`;
   }
 }
--- src/users/delete-user/delete-user-service.ts
+++ src/users/delete-user/delete-user-service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
 export class DeleteUserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  async deleteUser(id: string): Promise<void> {
+  async deleteUser(id: string, confirmationPhrase: string): Promise<void> {
+
     try {
-      await this.prisma.user.delete({
-        where: {
-          id: parseInt(id),
-        },
-      });
+
+      if (!confirmationPhrase) {
+        throw new BadRequestException({ message: 'Para exclusão da conta é obrigatório inserir a frase de confirmação de exclusão' });
+
+      } else {
+
+        await this.prisma.user.delete({
+          where: {
+            id: parseInt(id),
+          },
+        });
+      }
     } catch (error) {
       throw new Error(`Erro ao excluir usuário: ${error.message}`);
     }

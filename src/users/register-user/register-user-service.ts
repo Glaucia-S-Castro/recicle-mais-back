d3be2c80b7daa6e33cba582630eb3f3524f2ac7c
Glaucia Castro <galcastrossc@gmail.com>
2024-04-01 21:09:15 -0300

    refactoring of register-user controler and service, inclusion of new fields

--- src/users/register-user/register-user-service.ts
+++ src/users/register-user/register-user-service.ts
@@ -45,47 +45,4 @@ export class UserService {
     return (await this.users).find((user) => user.fullname === fullname);
   }
 
-  // async updateUser(data: UserDTO) {
-  //   if (!data.id) {
-  //     throw new BadRequestException({
-  //       message: 'Usuário não encontrado.',
-  //     });
-  //   }
-
-  //   const userExist = await this.prisma.user.findFirst({
-  //     where: {
-  //       id: data.id,
-  //     },
-  //   });
-
-  //   if (!userExist) {
-  //     throw new ConflictException({
-  //       message: 'Usuário não encontrado..',
-  //     });
-  //   }
-
-  //   if (data.email && data.email !== userExist.email) {
-  //     const userWithEmail = await this.prisma.user.findFirst({
-  //       where: { email: data.email },
-  //     });
-
-  //     if (userWithEmail) {
-  //       throw new ConflictException({
-  //         message: 'Este email já está sendo utilizado por outro usuário.',
-  //       });
-  //     }
-  //   }
-
-  //   const updatedUser = await this.prisma.user.update({
-  //     where: { id: data.id },
-  //     data: {
-  //       ...data,
-  //       password: data.password
-  //         ? await bcrypt.hash(data.password, this.saltOrRounds)
-  //         : userExist.password,
-  //     },
-  //   });
-
-  //   return updatedUser;
-  // }
 }
